4e8333e4fa0fbfda3c9ab722d7d9a85d83a85b2f
elcrack2001 <56980361+elcrack2001@users.noreply.github.com>
2019-10-27 18:34:18 -0500

    Add files via upload

---
+++ Librerias.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <conio.h>
+#include <fstream>
+#include <stdio.h>
+
+#include <string>
+#include <msclr/marshal_cppstd.h>
+
+#include "ArbolHeadMAX.h"
+#include "ArbolHeadMIN.h"
